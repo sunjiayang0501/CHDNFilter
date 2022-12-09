ed231977b6f1093e673e61a0b47de26074683b9b
Jiayang_Sun <57060099+sunjiayang0501@users.noreply.github.com>
2022-12-09 11:21:47 -0500

    Update data.R

--- R/data.R
+++ R/data.R
@@ -1,1 +1,34 @@
+#' VCF files of different individuals
+#'
+#' Individual1: 21B02337260
+#'
+#'
+#' @format VCFv4.2
+#' @examples
+#' \dontrun{
+#'  individual1
+#' }
+"individual1"
+
+#' Individual2: 21B02337261
+#'
+#'
+#' @format VCFv4.2
+#' @examples
+#' \dontrun{
+#'  individual2
+#' }
+"individual2"
+
+#' Individual3: 21B02337262
+#'
+#'
+#' @format VCFv4.2
+#' @examples
+#' \dontrun{
+#'  individual3
+#' }
+"individual3"
+
+# [END]
 
